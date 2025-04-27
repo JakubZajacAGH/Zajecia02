37a9af0051fc8379bed5c7b7e48748f7cb7ef0f8
JakubZajacAGH <zajacjakub@student.agh.edu.pl>
2025-04-27 10:25:47 +0200

    Update Order.cs

--- Order.cs
+++ Order.cs
@@ -4,4 +4,11 @@
     public DateTime OrderDate { get; set; }
     public decimal TotalAmount { get; set; }
     public int CustomerId { get; set; }
+    /// <summary>
+    /// 1 - Draft,
+    /// 2 - Submitted,
+    /// 3 - Completed,
+    /// 4 - Archived
+    /// </summary>
+    public int Status { get; set; }
 }
